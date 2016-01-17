5b0dae28481e7b5d5eb2bde8c9511c1786f69da6
Odyseja1602 <odyseja16@gmail.com>
2016-01-17 23:18:07 +0100

    first steps

--- src/main/scala/simplifier/simplifier.scala
+++ src/main/scala/simplifier/simplifier.scala
@@ -1,5 +1,7 @@
 package simplifier
 
+import javax.security.auth.login.FailedLoginException
+
 import AST._
 
 // to implement
@@ -7,6 +9,26 @@ import AST._
 // take into account non-greedy strategies to resolve cases with power laws
 object Simplifier {
 
-  def simplify(node: Node) = node
+  //def simplify(node: Node) = node
 
+  def simplify(node: Node): Node = node match {
+    case BinExpr(op, left, right) => {
+      val leftNode: Node = simplify(left)
+      val rightNode: Node = simplify(right)
+
+      op match {
+        case "and" => and(leftNode, rightNode)
+        case _ => node
+      }
+
+    }
+    case NodeList(list) => NodeList(list.map{case f => simplify(f)})
+    case IfInstr(cond, body) => IfInstr(simplify(cond), simplify(body))
+    case _ => node
+  }
+  def and(left: Node, right: Node):Node = (left, right) match {
+    case(_, FalseConst()) => FalseConst()
+    case(FalseConst(), _) => FalseConst()
+    case(_, _) => BinExpr("and", left, right)
+  }
 }
